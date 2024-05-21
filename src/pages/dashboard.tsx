1f6251b28b927be9b900a02b42eb0c6842e13485
Wellington Pereira <lleco12@gmail.com>
2024-05-21 14:48:49 -0300

    notificação

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -32,6 +32,20 @@ export default function Dashboard() {
     // Redirecione o usuário de volta para a página inicial
     router.push('/');
   };
+
+  const data = [
+    { title: "Item econtrado: variavel ", description: "dados a mais", year: "21/05/2007 14:44" },
+    { title: "Item econtrado: variavel ", description: "dados a mais", year: "21/05/2007 14:00" },
+    { title: "Item econtrado: variavel ", description: "dados a mais", year: "21/05/2007 13:35" },
+    { title: "Item econtrado: variavel ", description: "dados a mais", year: "21/05/2007 12:00" },
+    { title: "Item econtrado: variavel ", description: "dados a mais", year: "21/05/2007 10:44" },
+    { title: "Item econtrado: variavel ", description: "dados a mais", year: "21/05/2007 08:00" },
+    { title: "Item econtrado: variavel ", description: "dados a mais", year: "21/05/2007 01:44" },
+    { title: "Item econtrado: variavel ", description: "dados a mais", year: "21/05/2007 00:30" },
+    { title: "Item econtrado: variavel ", description: "dados a mais", year: "20/05/2007 14:44" },
+    // Outros objetos JSON aqui
+  ];
+  
   return (
     <div>
       <Head>
@@ -228,13 +242,24 @@ export default function Dashboard() {
       </header>
       <main>
       <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        {/* Replace with your content */}
-        <div className="px-4 py-6 sm:px-0">
-          <div className="border-4 border-dashed border-gray-200 rounded-lg h-96 flex items-center justify-center">
-            <p className="text-gray-500 text-center">Seu conteúdo aqui</p>
+        {/* começo notificação */}
+        <div className="flex items-top justify-center bg-white px-6 md:px-60 h-screen">
+      <div className="space-y-6 border-l-2 border-dashed">
+        {data.map((item, index) => (
+          <div key={index} className="relative w-full">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="absolute -top-0.5 z-10 -ml-3.5 h-7 w-7 rounded-full text-blue-500">
+              <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
+            </svg>
+            <div className="ml-6">
+              <h4 className="font-bold text-blue-500">{item.title}</h4>
+              <p className="mt-2 max-w-screen-sm text-sm text-gray-500">{item.description}</p>
+              <span className="mt-1 block text-sm font-semibold text-blue-500">{item.year}</span>
+            </div>
           </div>
-        </div>
-        {/* /End replace */}
+        ))}
+      </div>
+    </div>
+        {/* fim notificação */}
       </div>
 </main>
 
