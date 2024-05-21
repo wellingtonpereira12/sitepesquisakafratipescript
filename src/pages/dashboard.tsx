59ea77d9ebeba6bb96c246abbd71339c01060ee7
Wellington Pereira <lleco12@gmail.com>
2024-05-21 14:35:21 -0300

    meio

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -227,14 +227,17 @@ export default function Dashboard() {
         </div>
       </header>
       <main>
-        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-          {/* Replace with your content */}
-          <div className="px-4 py-6 sm:px-0">
-            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+      <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        {/* Replace with your content */}
+        <div className="px-4 py-6 sm:px-0">
+          <div className="border-4 border-dashed border-gray-200 rounded-lg h-96 flex items-center justify-center">
+            <p className="text-gray-500 text-center">Seu conteúdo aqui</p>
           </div>
-          {/* /End replace */}
         </div>
-      </main>
+        {/* /End replace */}
+      </div>
+</main>
+
     </div>
   )
 }
