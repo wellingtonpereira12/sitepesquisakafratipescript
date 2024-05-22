6788fa5b7363493e9494fe7b8d8cabee3a24c161
Wellington Pereira <lleco12@gmail.com>
2024-05-22 18:24:43 -0300

    no message

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -253,9 +253,9 @@ export default function Dashboard() {
                     <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
                   </svg>
                   <div className="ml-6">
-                    <h4 className="font-bold text-blue-500">{`${tasks.nome}`}</h4>
-                    <p className="mt-2 max-w-screen-sm text-sm text-gray-500">{'Foi encontrado o item a venda que você esta procurando: '}</p>
-                    <span className="mt-1 block text-sm font-semibold text-blue-500">{task.year}</span>
+                    <h4 className="font-bold text-blue-500">{task.titulo}</h4>
+                    <p className="mt-2 max-w-screen-sm text-sm text-gray-500">{task.descricao}</p>
+                    <span className="mt-1 block text-sm font-semibold text-blue-500">{task.data}</span>
                   </div>
                 </div>
               )  
